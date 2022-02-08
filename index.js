0fef182aa248edaac69a09b6123d5687c2a48d05
Kartikey Gupta <kartikeyrajgupta007@gmail.com>
2022-02-08 13:55:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let count = 0;
+let countp=document.getElementById("count-el")
+function increment(){
+    count++
+    countp.innerText = count
+}
+increment()
+function decrement(){
+    count--
+    countp.innerText = count
+}
+decrement()
+
+
+
+
+
+
+
+
+
+//document.getElementById("count-el").innerText = count
